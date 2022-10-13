eb0e8c60ae01fa5c137cf9dae9ab0587c73553e5
ysabel <regina.pujay@gmail.com>
2022-10-13 17:40:48 -0500

    add cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import products from './routes/products.routes.js';
-
+import cors from "cors";
 const app = express();
+
+app.use(cors());
+
 //routes
 app.use('/api', products);
 
